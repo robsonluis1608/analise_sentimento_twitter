8ff587bb5ea2988752baa428fd762ee04f132b22
robsonluis1608 <90524131+robsonluis1608@users.noreply.github.com>
2022-11-26 00:31:51 +0000

    update

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ import plotly.graph_objects as go
 from googletrans import Translator
 from stop_word import stop_palavras
 import plotly.express as px
-nltk.download('vader_lexicon')  
+nltk.download('all')  
 
 # chaves de acesso
 consumer_key = st.secrets['ck']
